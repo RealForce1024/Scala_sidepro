c9f58f89b5ccb7896021d7f89ac7ebbfb88c3591
fqc <337940626@qq.com>
2016-07-15 12:33:00 +0800

    包访问权限private[packageName] class xxx，以及private[this]类访问 private伴生对象访问

--- src/ch06/Person.scala
+++ src/ch06/Person.scala
@@ -3,7 +3,7 @@ package ch06
 /**
   * Created by fqc on 2016/7/14.
   */
-class Person(var id:Int= 0,var age:Int = 0) {
+private[ch06] class Person(var id:Int= 0,var age:Int = 0) {//包访问权限，所有子包可以访问，其他包则无法访问
   val name: String = null //字段需要进行初始化
   //javap -private Person
   /* public class ch06.Person {
@@ -12,8 +12,9 @@ class Person(var id:Int= 0,var age:Int = 0) {
      public ch06.Person();
    }*/
 
-  private[this] var gender :String = _ //只有当前类中才能够使用  _只有new的时候初始化相当于null
-  private var country: String = "china"
+  private[this] var gender :String = _ //只有当前类中才能够使用 伴生对象是无法访问的，可以定义公有方法访问 _只有new的时候初始化相当于null
+
+  private var country: String = "china" //只能在伴生对象中访问，其他对象或类时无法访问到的
   /*public class ch06.Person {
     private final java.lang.String name;
     private java.lang.String country;
