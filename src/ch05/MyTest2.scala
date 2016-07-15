b70525e5cca91a371d8f003a139e76676232d9d8
fqc <337940626@qq.com>
2016-07-15 11:07:38 +0800

    重构 并行wordcount操作 单机

--- src/ch05/MyTest2.scala
+++ src/ch05/MyTest2.scala
@@ -38,9 +38,10 @@ object MyTest2 {
     */
 
     //将Iterator转换成List
-    val lst5: List[List[Int]] = lst0.grouped(4).toList //分组的iterator转为list操作很方便
+    val lst5: List[List[Int]] = lst0.grouped(4).toList //分组的iterator转为list操作很方便 ,iterator与list可以互相转换
     println(lst5) //List(List(1, 7, 9, 8), List(0, 3, 5, 4), List(6, 2))
     println(lst5.flatten) //List(1, 7, 9, 8, 0, 3, 5, 4, 6, 2)
+
     //将多个list压扁成一个List
     val lines = List("hello tom hello jerry", "hello jerry", "hello kitty")
     //先按空格切分，在压平
@@ -87,22 +88,43 @@ object MyTest2 {
     //    println(words.map(_.split(" ")).flatten.toList.map(x => (x, 1)).groupBy(_._1).toList.map(_ => _._2.size))
     //    println(words.map(_.split(" ")).flatten.toList.map(x => (x, 1)).groupBy(_._1).toList.map((_._2.size)))
 
-    println(words.map(_.split(" ")).flatten.toList.map(x => (x, 1)).groupBy(_._1).toList.map(x => (x._1, x._2.size)))//较为啰嗦，toList ，map flatten可以使用flatMap代替
+    println(words.map(_.split(" ")).flatten.toList.map(x => (x, 1)).groupBy(_._1).toList.map(x => (x._1, x._2.size))) //较为啰嗦，toList ，map flatten可以使用flatMap代替
     //List((fqc,2), (kobe,1), (tom,1), (jim,1), (hello,4))
 
 
-    println(words.map(_.split(" ")).flatten.toList.map((_,1)))
-    println(words.map(_.split(" ")).flatten.map((_,1)))
-    println(words.flatMap(_.split(" ")).map((_,1)).groupBy(_._1).map(x=>(x._1,x._2.size)))
+    println(words.map(_.split(" ")).flatten.toList.map((_, 1)))
+    println(words.map(_.split(" ")).flatten.map((_, 1)))
+    println(words.flatMap(_.split(" ")).map((_, 1)).groupBy(_._1).map(x => (x._1, x._2.size)))
 
     //对结果排序
-    println(words.flatMap(_.split(" ")).map((_,1)).groupBy(_._1).map(x=>(x._1,x._2.size)).toList.sortBy(_._2))
-    println(words.flatMap(_.split(" ")).map((_,1)).groupBy(_._1).map(x=>(x._1,x._2.size)).toList.sortBy(_._2).reverse)
+    println(words.flatMap(_.split(" ")).map((_, 1)).groupBy(_._1).map(x => (x._1, x._2.size)).toList.sortBy(_._2))
+    println(words.flatMap(_.split(" ")).map((_, 1)).groupBy(_._1).map(x => (x._1, x._2.size)).toList.sortBy(_._2).reverse)
 
     //mapValues更加精炼
-    println(words.flatMap(_.split(" ")).map((_,1)).groupBy(_._1).mapValues(x=>x.size))
-    println(words.flatMap(_.split(" ")).map((_,1)).groupBy(_._1).mapValues(_.size))
+    println(words.flatMap(_.split(" ")).map((_, 1)).groupBy(_._1).mapValues(x => x.size))
+    println(words.flatMap(_.split(" ")).map((_, 1)).groupBy(_._1).mapValues(_.size)) //直接对对偶元素的value进行操作，key保持不动
+
+    //但是上面有一个特大的局限就是 mapValues或map时都使用的size，如果分组局部间进行聚合优化了，size将是错误的。 即局部combiner
+    //先看下基础的
+    val arrsum = Array(1, 2, 3, 4, 5, 6)
+    println(arrsum.sum)
+    println(arrsum.reduce(_ + _)) // 21 前面结果依次累加后面的一个元素 默认调用的reduceLeft. 注意是单线程的，如果数据量大则需要并行
+
+    println(arrsum.par.reduce(_ + _)) //21 具体看机器cpu核数
+
+    //println(arrsum.fold(_+_))需要有合并的默认值
+    //使用fold进行并行计算
+    println(arrsum.fold(10)(_ + _)) //31
+    println(arrsum.par.fold(10)(_ + _)) //71 81 61 都能够跑出来 这是涉及并发问题，怎样解决？
+    println(arrsum.par.fold(0)(_ + _)) //21 将初始值设置为0，就避免了上面的问题
+
+    println(arrsum.foldLeft(0)(_ + _)) //21
+    println(arrsum.foldRight(10)(_ + _)) //31
 
 
+    //再次重构wordcount
+    println(words.flatMap(_.split(" ")).map((_, 1)).groupBy(_._1).mapValues(_.foldLeft(0)(_ + _._2)))
+    println(words.flatMap(_.split(" ")).map((_, 1)).groupBy(_._1).mapValues(_.par.foldLeft(0)(_ + _._2)))//单机版并行wordcount操作
   }
+
 }
