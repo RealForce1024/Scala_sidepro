4dd2550695c5713cb75a27b81882585659c77436
fqc <337940626@qq.com>
2016-07-16 10:33:38 +0800

    匹配则封装到Some对象当中

--- src/mytest/casematch/OptionDemo.scala
+++ src/mytest/casematch/OptionDemo.scala
@@ -1,12 +1,15 @@
 package mytest.casematch
 
+/**
+  * option对象
+  */
 object OptionDemo {
   def main(args: Array[String]) {
     val map = Map("a" -> 1, "b" -> 2)
 
     val v = map.get("b") match {
-      case Some(i) => i
-      case None => 0
+      case Some(i) => i //匹配则封装到Some对象当中
+      case None => 0 //否则None对象
     }
     println(v)
     //更好的方式
