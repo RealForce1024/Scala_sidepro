8ee8c702e12e62a55d364f8003a638d50d626215
fqc <337940626@qq.com>
2016-07-16 14:02:55 +0800

    偏函数的变形

--- src/mytest/casematch/PartialFuncDemo.scala
+++ src/mytest/casematch/PartialFuncDemo.scala
@@ -7,7 +7,7 @@ package mytest.casematch
   */
 object PartialFuncDemo {
 
-  def func1: PartialFunction[String, Int] = {
+  def func1: PartialFunction[String, Int] = {//无match 有case的偏函数
     case "one" => {
       println("one case")
       1
@@ -30,9 +30,17 @@ object PartialFuncDemo {
     case _ => "不认识的"
   }
 
+  def func4(name: String) = name match{//可省略返回值 ，但还是写上最好 类型推导
+    case "kobe" => "科比"
+    case "james" => "詹姆斯"
+    case "jordan" => "乔丹"
+    case _ => "不认识"
+  }
+
   def main(args: Array[String]) {
     println(func1("two"))
     println(func2("one"))
     println(func3("kobe"))
+    println(func4("jordan"))
   }
 }
