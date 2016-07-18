ff45916f53f7262d366472e13133a5bb881a2f67
fqc <337940626@qq.com>
2016-07-18 22:44:35 +0800

    actor异步消息返回，上次提交 更正为actor版wordcount

--- src/actor/SyncMsgAndAyscMsg.scala
+++ src/actor/SyncMsgAndAyscMsg.scala
@@ -1,8 +1,43 @@
 package actor
 
+import scala.actors.Actor
+
 /**
   * Created by fqc on 2016/7/16.
   */
-class SyncMsgAndAyscMsg {
+class SyncMsgAndAyscMsg extends Actor {
+  override def act(): Unit = {
 
+    loop(
+      react {
+        case "start" => println("start")
+        case SyncMsg(id, msg) => {
+          println(s"id:$id , SyncMsg:$msg")
+          sender ! ReplyMsg(1,"finished")
+        }
+      }
+    )
+  }
+}
+
+
+case class SyncMsg(id: Int, msg: String) //封装数据
+
+case class AyncMsg(id: Int, msg: String)
+
+case class ReplyMsg(id: Int, msg: String)
+
+object AppRun extends App {
+
+  val msg: SyncMsgAndAyscMsg = new SyncMsgAndAyscMsg
+  msg.start() //start
+  val replyMsg= msg !? SyncMsg(10000, "syncMsg")
+  println(replyMsg)
+
+  val retMsg = msg !! SyncMsg(11111,"syncMsg")
+  println(retMsg)
+  println(retMsg.isSet)
+  val c = retMsg.apply
+  println(retMsg.isSet)
+  println(c)
 }
