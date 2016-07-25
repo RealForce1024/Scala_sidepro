ee860a3aff329b65791d20b61f6054a2b4462db7
fqc <337940626@qq.com>
2016-07-25 09:25:49 +0800

    隐式方法转换

---
+++ src/mytest/TestImplicitMethod.scala
@@ -0,0 +1,32 @@
+package mytest
+
+import java.io.File
+
+import scala.io.Source
+
+/**
+  * Created by fqc on 2016/7/25.
+  */
+class TestImplicitMethod {
+
+
+}
+
+//2. 包装类
+class RichFile(file: File) {
+  def myRead = Source.fromFile(file).getLines().mkString
+}
+
+//1. 定义隐式转换方法，该方法参数为被包装类
+object Context {
+  implicit def file2RichFile(file: File) = new RichFile(file)
+}
+
+object Test {
+  def main(args: Array[String]) {
+    //3. 导入隐式转换
+    import Context._
+    val file: File = new File("c:/1.log")
+    println(file.myRead)
+  }
+}
