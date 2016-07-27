515bae41b3f7a2781d99d427b70695c671540f18
fqc <337940626@qq.com>
2016-07-27 22:23:03 +0800

    Actor是Scala中的线程实现

--- src/actor/TwoThreadPrint.scala
+++ src/actor/TwoThreadPrint.scala
@@ -5,6 +5,7 @@ import scala.actors.Actor
 /**
   * Created by fqc on 2016/7/16.
   * 实现的功能与ThreadPrint一样，且极度相似。
+  * Actor是Scala中的线程实现
   */
 object T1 extends Actor {
   //Actor 是trait 其内是实现的act方法 当做实现的接口 ctrl+i 覆盖其方法
